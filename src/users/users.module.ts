39b94fc7f65902439a0f0cb2575d6417db90b204
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2026-06-30 11:56:50 -0300

    feat: Add Auth Configs

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersRepositoryTypeORM } from './users.repository';
+import { EncryptService } from '../common/encrypt.service';
+import { AuthenticationModule } from '../authentication/authentication.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthenticationModule),
+  ],
   controllers: [UsersController],
   providers: [
     UsersService,
@@ -14,6 +19,8 @@ import { UsersRepositoryTypeORM } from './users.repository';
       provide: 'UsersRepository',
       useClass: UsersRepositoryTypeORM,
     },
+    EncryptService,
   ],
+  exports: [UsersModule, UsersService],
 })
 export class UsersModule {}
