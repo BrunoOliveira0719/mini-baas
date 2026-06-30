96769351fae64f0361dc7547eba8d85c8e130261
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2026-06-30 12:02:01 -0300

    feat: Create Find User by Email for Test Protect Route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,47 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res, UseGuards, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import type { Response } from 'express';
+import { Public } from '../common/decorators/public.decorator';
+import { JwtAuthGuard } from '../authentication/guards/jwt-auth.gaurd';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { user, accessToken, refreshToken } =
+      await this.usersService.create(createUserDto);
+
+    const { password, ...userWithoutPassword } = user;
+
+    response.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: Number(process.env.EXPIRES_ACCESS_TOKEN) * 60 * 1000,
+    });
+
+    response.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: Number(process.env.EXPIRES_REFRESH_TOKEN) * 60 * 1000,
+    });
+
+    return { message: 'User created successfully!', userWithoutPassword };
+  }
+
+  @Get()
+  async findByEmail(@Body('email') email: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    return user;
   }
 }
